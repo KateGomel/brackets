cfaa6ff98683b2cddaba778ba54fba0023d395b4
KateGomel <ykatuay@gmail.com>
2024-08-11 14:07:30 +0300

    feat: add temp solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pairsBrackets = {};
+  bracketsConfig.map((item) => {
+    pairsBrackets[item[1]] = item[0];
+  });
+
+  let closeBrackets = Object.keys(pairsBrackets);
+
+  let openBrackets = Object.values(pairsBrackets);
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const temp = str[i];
+    if (openBrackets.includes(temp)) {
+      stack.push(temp);
+      if (pairsBrackets[temp] === temp) {
+        stack.pop();
+      }
+    } else if (closeBrackets.includes(temp)) {
+      if (pairsBrackets[temp] === stack[stack.length - 1]) {
+        stack.pop();
+      } else if (!stack.length || pairsBrackets[temp] !== stack.pop()) {
+        stack.push(temp);
+      }
+    }
+  }
+
+  if (stack.length) return false;
+  else return true;
+};
